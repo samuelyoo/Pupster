52717d92a33302655ee3a4fcf84b5c7fbcf63b09
Samuel Yoo <samuel.hc.yoo@forso.net>
2020-07-23 21:42:43 -0400

    added anothe2r /

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <Router baseName ="/Pupster/">
+    <Router baseName ="Pupster/">
       <div>
         <Navbar />
         <Wrapper>
