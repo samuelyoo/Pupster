10523a4c6e11b95e4366015925e71fea98850fe7
Samuel Yoo <samuel.hc.yoo@forso.net>
2020-07-23 21:29:15 -0400

    Updates

---
+++ precache-manifest.435128bde37738566c5ced5a4d80339e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f593655401ba8b246ccc0c70fdc9884",
+    "url": "/pupster/index.html"
+  },
+  {
+    "revision": "dbeeceba60377a6e1053",
+    "url": "/pupster/static/css/main.5161d8a5.chunk.css"
+  },
+  {
+    "revision": "50d3c2261a8524983eb4",
+    "url": "/pupster/static/js/2.c9b3c6d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pupster/static/js/2.c9b3c6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbeeceba60377a6e1053",
+    "url": "/pupster/static/js/main.2c6baca0.chunk.js"
+  },
+  {
+    "revision": "08e668c994ed8d910483",
+    "url": "/pupster/static/js/runtime-main.2d8bf639.js"
+  },
+  {
+    "revision": "c0f23a817df7477145eb40f162ebbb13",
+    "url": "/pupster/static/media/kuro_bg.c0f23a81.jpg"
+  }
+]);
